68a307522050f7f4b3f69802f0adaa1000b757df
Rocio Batres <jonasgirl21@gmail.com>
2020-12-05 06:30:06 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,6 +1,6 @@
 class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.2] 
   def change
-    change_column (:students, :datetime, string)
+    change_column (:students, :datetime, datetime)
     end 
   end 
 end 
